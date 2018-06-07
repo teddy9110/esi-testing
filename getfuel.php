a30a0afa93ee72feef85f4a40ab2d62081ac694c
U-AlexIles-PC\Alex.Iles <alexiles1995@gmail.com>
2018-06-07 16:44:14 +0100

    adding component files

---
+++ getfuel.php
@@ -0,0 +1,43 @@
+<?PHP
+
+function getfuel($response){
+
+
+
+//print_r(array_values($response));
+
+
+$key =  array_column($response, 'fuel_expires');
+
+
+
+$size = count($key);
+
+
+
+$i = 0;
+$storesdates = implode(" ",$key);
+//echo (trim($storesdates, " "));
+echo  nl2br ("\n");
+echo  nl2br ("\n");
+
+
+//print_r($storedates);
+
+
+foreach ($key as $size) {
+  // code...
+
+
+
+  $display = strtotime($key[$i]);
+  //echo $display;
+
+  $unixtime = $display;
+  echo $time = date("d/m/y h:i:s A T",$unixtime);
+  echo  nl2br ("\n");
+  $i++;
+
+
+}
+}
